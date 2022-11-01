0d2c647e752fc4cb06302c03d52b93281521be35
kirangond <kirangond05@gmail.com>
2022-11-01 22:51:24 +0530

    added reverse the array class

---
+++ dsa_programs/src/array_log2base2/ReverseTheArray.java
@@ -0,0 +1,18 @@
+package array_log2base2;
+
+public class ReverseTheArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {2, 1, 3, 4};
+		int n=4,i;
+		 for(i=n-1;i>=0;i--)
+		    {
+		       System.out.println(arr[i]);
+		    }
+		    
+
+	}
+
+}
